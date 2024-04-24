62b6a9d227b72c80d2c6d4c4c37f4e1e132b487c
Neo-Diamons <romain.hure@epitech.eu>
2024-04-24 05:00:24 +0200

    refactor(parser): clean code.

--- src/Core/src/Parser/Parser.cpp
+++ src/Core/src/Parser/Parser.cpp
@@ -13,116 +13,109 @@
 #include <libconfig.h++>
 #include <string>
 
-using namespace rt;
-
-std::string Parser::getLibPathFromMainBinary(const std::string &path)
+namespace rt
 {
-    std::string binaryPath = std::filesystem::read_symlink("/proc/self/exe");
-    std::string binaryDir = std::filesystem::path(binaryPath).parent_path();
-    std::string finalPath = binaryDir + "/lib/" + path;
+    std::string Parser::getLibPathFromMainBinary(const std::string &path)
+    {
+        const std::string binaryPath = std::filesystem::read_symlink("/proc/self/exe");
+        const std::string binaryDir = std::filesystem::path(binaryPath).parent_path();
+        std::string finalPath = binaryDir + "/lib/" + path;
 
-    return finalPath;
-}
+        return finalPath;
+    }
 
-void Parser::parseCamera(libconfig::Setting &camera)
-{
-    cameraLoaders.emplace_back(getLibPathFromMainBinary(camera["lib"]), "createComponent");
-    std::function<ICamera *(libconfig::Setting &)> createComponent = reinterpret_cast<ICamera *(
-        *)(libconfig::Setting &)>(cameraLoaders.back().get());
+    void Parser::parseCamera(libconfig::Setting &camera)
+    {
+        cameraLoaders.emplace_back(getLibPathFromMainBinary(camera["lib"]), "createComponent");
 
-    if (createComponent == nullptr)
-        throw std::runtime_error("Failed to load camera component");
+        const std::function createComponent = reinterpret_cast<ICamera *(*)(libconfig::Setting &)>
+            (cameraLoaders.back().get());
+        if (createComponent == nullptr)
+            throw std::runtime_error("Failed to load camera component");
 
-    _camera = createComponent(camera);
-}
+        _camera = createComponent(camera);
+    }
 
-void Parser::parseMaterials(libconfig::Setting &materials)
-{
-    for (int i = 0; i < materials.getLength(); i++) {
-        std::string materialName = static_cast<std::string>(materials[i]["name"]);
-        bool materialExists = false;
+    void Parser::parseMaterials(const libconfig::Setting &materials)
+    {
+        for (uint8_t i = 0; i < static_cast<uint8_t>(materials.getLength()); i++) {
+            auto materialName = static_cast<std::string>(materials[i]["name"]);
 
-        for (const auto &name : materialLoadersNames) {
-            if (name == materialName) {
-                materialExists = true;
-                break;
+            for (const auto &name : materialLoadersNames)
+                if (name == materialName)
+                    throw std::runtime_error("Material with the same name already exists: " + materialName);
+
+            try {
+                materialLoaders.emplace_back(getLibPathFromMainBinary(materials[i]["lib"]), "createComponent");
+                materialLoadersNames.emplace_back(materialName);
+            } catch (const utils::DLLoader<IMaterial>::DLLoaderException &e) {
+                std::cerr << e.what() << std::endl;
+                continue;
             }
+
+            const auto createComponent = reinterpret_cast<IMaterial *(*)(libconfig::Setting &)>
+                (materialLoaders.back().get());
+            if (createComponent == nullptr)
+                throw std::runtime_error("Failed to load material component");
+
+            _materials.emplace(materialName, createComponent(materials[i]));
         }
-        if (materialExists)
-            throw std::runtime_error("Material with the same name already exists: " + materialName);
-
-        try {
-            materialLoaders.emplace_back(getLibPathFromMainBinary(materials[i]["lib"]), "createComponent");
-            materialLoadersNames.emplace_back(materialName);
-        } catch (const utils::DLLoader<IMaterial>::DLLoaderException &e) {
-            std::cerr << e.what() << std::endl;
-            continue;
-        }
-        std::function<IMaterial *(libconfig::Setting &)> createComponent;
-        try {
-            createComponent = reinterpret_cast<IMaterial *(*)(libconfig::Setting &)>(materialLoaders.back().get());
-        } catch (const std::out_of_range &e) {
-            throw std::runtime_error("Material not found: " + materialName);
-        }
-
-        if (createComponent == nullptr)
-            throw std::runtime_error("Failed to load material component");
-
-        _materials.emplace(materialName, createComponent(materials[i]));
-    }
-}
-
-void Parser::parsePrimitives(libconfig::Setting &primitives)
-{
-    for (int i = 0; i < primitives.getLength(); i++) {
-        IMaterial *usedMaterial = nullptr;
-
-        try {
-            usedMaterial = _materials.at(static_cast<std::string>(primitives[i]["material"]));
-        } catch (const std::out_of_range &e) {
-            throw std::runtime_error("Material not found: " + static_cast<std::string>(primitives[i]["material"]));
-        }
-
-        try {
-            primitiveLoaders.emplace_back(getLibPathFromMainBinary(primitives[i]["lib"]), "createComponent");
-        } catch (const utils::DLLoader<IPrimitive>::DLLoaderException &e) {
-            std::cerr << e.what() << std::endl;
-            continue;
-        }
-        std::function<IPrimitive *(libconfig::Setting &, IMaterial *)> createComponent = reinterpret_cast<IPrimitive *(
-            *)(libconfig::Setting &, IMaterial *)>(primitiveLoaders.back().get());
-
-        if (createComponent == nullptr)
-            throw std::runtime_error("Failed to load primitive component");
-
-        _primitives.emplace_back(createComponent(primitives[i], usedMaterial));
-    }
-}
-
-Parser *Parser::parseScene(const std::string &path)
-{
-    libconfig::Config cfg;
-    std::string abs_path = std::filesystem::absolute(path);
-
-    try {
-        cfg.readFile(abs_path);
-        libconfig::Setting &root = cfg.getRoot();
-        parseCamera(root["camera"]);
-        parseMaterials(root["materials"]);
-        parsePrimitives(root["primitives"]);
-        std::cout << "Scene parsed successfully" << std::endl;
-    } catch (const libconfig::FileIOException &fioex) {
-        std::cerr << "I/O error while reading file: " << fioex.what() << std::endl;
-    } catch (const libconfig::ParseException &pex) {
-        std::cerr << "Parse error at " << pex.getFile() << ":" << pex.getLine()
-            << " - " << pex.getError() << std::endl;
-    } catch (const libconfig::SettingNotFoundException &nfex) {
-        std::cerr << "Setting not found: " << nfex.getPath() << std::endl;
-    } catch (const libconfig::SettingTypeException &stex) {
-        std::cerr << "Setting type error: " << stex.getPath() << std::endl;
-    } catch (const std::exception &e) {
-        std::cerr << "ERROR : " << e.what() << std::endl;
     }
 
-    return this;
+    void Parser::parsePrimitives(const libconfig::Setting &primitives)
+    {
+        for (uint8_t i = 0; i < static_cast<uint8_t>(primitives.getLength()); i++) {
+            IMaterial *usedMaterial;
+
+            try {
+                usedMaterial = _materials.at((primitives[i]["material"]));
+            } catch (const std::out_of_range &) {
+                throw std::runtime_error("Material not found: " + static_cast<std::string>(primitives[i]["material"]));
+            }
+
+            try {
+                primitiveLoaders.emplace_back(getLibPathFromMainBinary(primitives[i]["lib"]), "createComponent");
+            } catch (const utils::DLLoader<IPrimitive>::DLLoaderException &e) {
+                std::cerr << e.what() << std::endl;
+                continue;
+            }
+
+            std::function createComponent = reinterpret_cast<IPrimitive *(*)(libconfig::Setting &, IMaterial *)>
+                (primitiveLoaders.back().get());
+            if (createComponent == nullptr)
+                throw std::runtime_error("Failed to load primitive component");
+
+            _primitives.emplace_back(createComponent(primitives[i], usedMaterial));
+        }
+    }
+
+    Parser *Parser::parseScene(const std::string &path)
+    {
+        const std::string abs_path = std::filesystem::absolute(path);
+
+        try {
+            libconfig::Config cfg;
+            cfg.readFile(abs_path);
+
+            const libconfig::Setting &root = cfg.getRoot();
+            parseCamera(root["camera"]);
+            parseMaterials(root["materials"]);
+            parsePrimitives(root["primitives"]);
+
+            std::cout << "Scene parsed successfully" << std::endl;
+        } catch (const libconfig::FileIOException &fioex) {
+            std::cerr << "I/O error while reading file: " << fioex.what() << std::endl;
+        } catch (const libconfig::ParseException &pex) {
+            std::cerr << "Parse error at " << pex.getFile() << ":" << pex.getLine()
+                << " - " << pex.getError() << std::endl;
+        } catch (const libconfig::SettingNotFoundException &nfex) {
+            std::cerr << "Setting not found: " << nfex.getPath() << std::endl;
+        } catch (const libconfig::SettingTypeException &stex) {
+            std::cerr << "Setting type error: " << stex.getPath() << std::endl;
+        } catch (const std::exception &e) {
+            std::cerr << "ERROR : " << e.what() << std::endl;
+        }
+
+        return this;
+    }
 }
--- src/Core/src/Parser/Parser.hpp
+++ src/Core/src/Parser/Parser.hpp
@@ -6,48 +6,49 @@
 */
 
 #ifndef PARSER_HPP_
-    #define PARSER_HPP_
+#define PARSER_HPP_
 
-    #include <libconfig.h++>
-    #include <map>
+#include <libconfig.h++>
+#include <map>
 
-    #include "../../../Cameras/ICamera.hpp"
-    #include "../../../Plugins/Primitives/IPrimitive.hpp"
-    #include "../../../Plugins/Materials/IMaterial.hpp"
-    // #include "../../Plugins/Lights/ILight.hpp"
+#include "../../../Cameras/ICamera.hpp"
+#include "../../../Plugins/Primitives/IPrimitive.hpp"
+#include "../../../Plugins/Materials/IMaterial.hpp"
+// #include "../../Plugins/Lights/ILight.hpp"
 
-namespace rt {
+namespace rt
+{
     class Parser {
-        public:
-            Parser() = default;
-            ~Parser() {};
+    public:
+        Parser() = default;
+        ~Parser() {};
 
-            rt::ICamera *getCamera(void) { return _camera; }
-            std::list<rt::IPrimitive *> getPrimitives(void) { return _primitives; }
-            // std::list<rt::ILight *> getLights(void) { return _lights; }
+        ICamera *getCamera(void) { return _camera; }
+        std::list<IPrimitive *> getPrimitives(void) { return _primitives; }
+        // std::list<ILight *> getLights(void) { return _lights; }
 
-            Parser *parseScene(const std::string &path);
+        Parser *parseScene(const std::string &path);
 
-            private:
-                static std::string getLibPathFromMainBinary(const std::string &path);
+    private:
+        static std::string getLibPathFromMainBinary(const std::string &path);
 
-                void parseCamera(libconfig::Setting &camera);
-                void parsePrimitives(libconfig::Setting &primitives);
-                void parseMaterials(libconfig::Setting &materials);
-                // void parseLights(libconfig::Setting &lights);
+        void parseCamera(libconfig::Setting &camera);
+        void parsePrimitives(const libconfig::Setting &primitives);
+        void parseMaterials(const libconfig::Setting &materials);
+        // void parseLights(libconfig::Setting &lights);
 
-                rt::ICamera *_camera{};
-                std::list<rt::IPrimitive *> _primitives;
-                // std::list<rt::ILight *> _lights;
+        ICamera *_camera{};
+        std::list<IPrimitive *> _primitives;
+        // std::list<ILight *> _lights;
 
-                std::map<std::string, rt::IMaterial *> _materials;
+        std::map<std::string, IMaterial *> _materials;
 
-                std::list<utils::DLLoader<ICamera>> cameraLoaders;
-                std::list<utils::DLLoader<IPrimitive>> primitiveLoaders;
-                // std::vector<utils::DLLoader<ILight>> lightLoaders;
+        std::list<utils::DLLoader<ICamera>> cameraLoaders;
+        std::list<utils::DLLoader<IPrimitive>> primitiveLoaders;
+        // std::vector<utils::DLLoader<ILight>> lightLoaders;
 
-                std::list<std::string> materialLoadersNames;
-                std::list<utils::DLLoader<IMaterial>> materialLoaders;
+        std::list<std::string> materialLoadersNames;
+        std::list<utils::DLLoader<IMaterial>> materialLoaders;
     };
 }
 
