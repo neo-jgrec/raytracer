7781dc76a98a60a2f5eb30df9e2920d96341a499
Neo-Diamons <romain.hure@epitech.eu>
2024-04-26 17:29:53 +0200

    feat(tetrahedron): add tetrahedron shape.

---
+++ src/Plugins/Primitives/Tetrahedron/src/Tetrahedron.cpp
@@ -0,0 +1,51 @@
+/*
+** EPITECH PROJECT, 2024
+** raytracer
+** File description:
+** Tetrahedron
+*/
+
+#include "Tetrahedron.hpp"
+
+#include <vector>
+
+namespace rt
+{
+    float Tetrahedron::hit(const math::Ray &ray) const
+    {
+        constexpr float epsilon = std::numeric_limits<float>::epsilon();
+        const std::vector<std::tuple<math::Vector3<float>, math::Vector3<float>, math::Vector3<float>>> edges = {
+            {_v0, _v1, _v2}, {_v0, _v1, _v3}, {_v0, _v2, _v3}, {_v1, _v2, _v3}};
+        float t_min = -1;
+
+        for (uint8_t i = 0; i < 4; i++) {
+            const math::Vector3<float> edge1 = std::get<1>(edges[i]) - std::get<0>(edges[i]);
+            const math::Vector3<float> edge2 = std::get<2>(edges[i]) - std::get<0>(edges[i]);
+            const math::Vector3<float> cross_edge2 = ray.direction.cross(edge2);
+            const float det = edge1.dot(cross_edge2);
+
+            // Ray is parallel to the triangle
+            if (det > -epsilon && det < epsilon)
+                continue;
+
+            // Ray intersects the rectangle containing the triangle
+            const float invDet = 1.0f / det;
+            const math::Vector3<float> tvec = ray.origin - std::get<0>(edges[i]);
+            const float u = tvec.dot(cross_edge2) * invDet;
+
+            if (u < 0 || u > 1)
+                continue;
+
+            // Ray intersects the triangle
+            const math::Vector3<float> cross_edge1 = tvec.cross(edge1);
+            const float v = ray.direction.dot(cross_edge1) * invDet;
+
+            if (v < 0 || u + v > 1)
+                continue;
+
+            if (const float t = ray.direction.dot(cross_edge1) * invDet; t > epsilon && t > t_min)
+                t_min = t;
+        }
+        return t_min;
+    }
+} // namespace rt
---
+++ src/Plugins/Primitives/Tetrahedron/src/Tetrahedron.hpp
@@ -0,0 +1,67 @@
+/*
+** EPITECH PROJECT, 2024
+** raytracer
+** File description:
+** Tetrahedron
+*/
+
+#ifndef SPHERE_HPP
+#define SPHERE_HPP
+
+#include <libconfig.h++>
+#include "../../APrimitive.hpp"
+
+namespace rt
+{
+    class Tetrahedron final : public APrimitive {
+    private:
+        math::Vector3<float> _v0;
+        math::Vector3<float> _v1;
+        math::Vector3<float> _v2;
+        math::Vector3<float> _v3;
+
+    public:
+        class TriangleException final : public APrimitiveException {
+        public:
+            TriangleException(const std::string &message) : APrimitiveException("Tetrahedron", message) {}
+        };
+
+        [[nodiscard]] float hit(const math::Ray &ray) const override;
+
+        void setV0(const math::Vector3<float> &v0) { _v0 = v0; }
+        void setV1(const math::Vector3<float> &v1) { _v1 = v1; }
+        void setV2(const math::Vector3<float> &v2) { _v2 = v2; }
+        void setV3(const math::Vector3<float> &v3) { _v3 = v3; }
+        [[nodiscard]] const math::Vector3<float> &getV0() const { return _v0; }
+        [[nodiscard]] const math::Vector3<float> &getV1() const { return _v1; }
+        [[nodiscard]] const math::Vector3<float> &getV2() const { return _v2; }
+        [[nodiscard]] const math::Vector3<float> &getV3() const { return _v3; }
+    };
+} // namespace rt
+
+extern "C" {
+    rt::IPrimitive *createComponent(const libconfig::Setting &setting, rt::IMaterial *material)
+    {
+        auto *tetrahedron = new rt::Tetrahedron();
+
+        tetrahedron->setV0({static_cast<float>(setting["v0"]["x"].operator int()),
+                            static_cast<float>(setting["v0"]["y"].operator int()),
+                            static_cast<float>(setting["v0"]["z"].operator int())});
+        tetrahedron->setV1({static_cast<float>(setting["v1"]["x"].operator int()),
+                            static_cast<float>(setting["v1"]["y"].operator int()),
+                            static_cast<float>(setting["v1"]["z"].operator int())});
+        tetrahedron->setV2({static_cast<float>(setting["v2"]["x"].operator int()),
+                            static_cast<float>(setting["v2"]["y"].operator int()),
+                            static_cast<float>(setting["v2"]["z"].operator int())});
+        tetrahedron->setV3({static_cast<float>(setting["v3"]["x"].operator int()),
+                            static_cast<float>(setting["v3"]["y"].operator int()),
+                            static_cast<float>(setting["v3"]["z"].operator int())});
+        tetrahedron->setMaterial(material);
+
+        return tetrahedron;
+    }
+
+    void destroy(const rt::IPrimitive *ptr) { delete ptr; }
+}
+
+#endif // SPHERE_HPP
