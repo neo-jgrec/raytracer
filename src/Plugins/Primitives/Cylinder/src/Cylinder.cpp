ec637b4b4df1af0a4e322a31c96094a47ade91c5
Raphael <raphael.mabille@epitech.eu>
2024-05-10 17:20:59 +0200

    fix(Cylinder): modify getNormal to handle rotation

--- src/Plugins/Primitives/Cylinder/src/Cylinder.cpp
+++ src/Plugins/Primitives/Cylinder/src/Cylinder.cpp
@@ -12,18 +12,16 @@ namespace rt
 {
     float Cylinder::hit(const math::Ray &ray) const
     {
-        // Convert Euler angles to rotation matrix
         math::Matrix3 rotationMatrix = math::Matrix3::eulerToMatrix(_rotation * M_PI / 180);
 
-        // Rotate the ray
         math::Ray rotatedRay;
         rotatedRay.origin = rotationMatrix * (ray.origin - _origin);
         rotatedRay.direction = rotationMatrix * ray.direction;
 
-        math::Vector3 oc = rotatedRay.origin;
+        math::Vector3 rayO = rotatedRay.origin;
         float a = rotatedRay.direction.dot(rotatedRay.direction) - pow(rotatedRay.direction.dot(_direction.normalize()), 2);
-        float b = 2.0 * (rotatedRay.direction.dot(oc) - (rotatedRay.direction.dot(_direction.normalize()) * oc.dot(_direction.normalize())));
-        float c = oc.dot(oc) - pow(oc.dot(_direction.normalize()), 2) - _radius * _radius;
+        float b = 2.0 * (rotatedRay.direction.dot(rayO) - (rotatedRay.direction.dot(_direction.normalize()) * rayO.dot(_direction.normalize())));
+        float c = rayO.dot(rayO) - pow(rayO.dot(_direction.normalize()), 2) - _radius * _radius;
 
         float discriminant = b * b - 4 * a * c;
         if (discriminant < 0)
@@ -52,9 +50,13 @@ namespace rt
 
     math::Vector3<float> Cylinder::getNormal(const math::Vector3<float> &point) const
     {
+        math::Matrix3 rotationMatrix = math::Matrix3::eulerToMatrix(_rotation);
+
         math::Vector3<float> normal = point - _origin;
         normal = normal - _direction.normalize() * normal.dot(_direction.normalize());
+        normal = rotationMatrix * normal;
         normal = normal.normalize();
+
         return normal;
     }
 } // namespace rt
