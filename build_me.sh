9180fa03924302526a6b629650395ebeb85b18da
Neo-Diamons <romain.hure@epitech.eu>
2024-05-16 17:49:05 +0200

    build(build_me.sh): build library and binary in the build folder.

--- build_me.sh
+++ build_me.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 SCRIPT_LOCATION="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+OUTPUT_DIR="$SCRIPT_LOCATION/build/bin"
+BUILD_MODE="Release"
 
-if [ ! -d "build" ]; then
-    mkdir build
+if [ ! -d "$OUTPUT_DIR" ]; then
+    mkdir -p "$OUTPUT_DIR"
 fi
 if [ "$#" -eq 1 ] && [ "$1" = "-d" ]; then
     echo "-- DEBUG MODE"
-    cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=$SCRIPT_LOCATION/. -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=$SCRIPT_LOCATION/./lib
-else
-    cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=$SCRIPT_LOCATION/. -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=$SCRIPT_LOCATION/./lib
+    BUILD_MODE="Debug"
 fi
 
+cmake -S . -B build -DCMAKE_BUILD_TYPE="$BUILD_MODE" -DCMAKE_RUNTIME_OUTPUT_DIRECTORY="$OUTPUT_DIR" -DCMAKE_LIBRARY_OUTPUT_DIRECTORY="$OUTPUT_DIR/lib"
 cmake --build build -j
